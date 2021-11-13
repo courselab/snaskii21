052de04a5dc32c93593eca50c595688127906cf9
M-A-rs <araujo_m_@hotmail.com>
2021-11-13 13:59:46 -0300

    Add sudo password as command-line argument

--- install_all.py
+++ install_all.py
@@ -1,6 +1,13 @@
+import argparse
+import os
 import subprocess
 
-def main() -> None:    
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--password", type=str, help="(Optional) sudo password, if it's required by your system to install packages")
+    args = parser.parse_args()
+    password = args.password
+
     commands = [
         "apt install automake autoconf -y",
         "apt install libncurses5-dev -y",
@@ -11,7 +18,8 @@ def main() -> None:
     ]
     
     for command in commands:
-        subprocess.run(command.split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        
+        command_to_run = f"echo {password} | sudo -S {command}" if password is not None else command
+        subprocess.run(command_to_run, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
 if __name__ == "__main__":
     main()
