c17657947b58c5a0eb17b9dddf8656f45214f415
M-A-rs <araujo_m_@hotmail.com>
2021-11-13 13:58:37 -0300

    Refactor Python installation script to remove code duplication

--- install_all.py
+++ install_all.py
@@ -1,12 +1,17 @@
 import subprocess
 
-def main() -> None:
-    subprocess.run("apt install automake autoconf -y".split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    subprocess.run("apt install libncurses5-dev -y".split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    subprocess.run("./autogen.sh".split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    subprocess.run("./configure".split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    subprocess.run("make", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    subprocess.run("make install", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+def main() -> None:    
+    commands = [
+        "apt install automake autoconf -y",
+        "apt install libncurses5-dev -y",
+        "./autogen.sh",
+        "./configure",
+        "make",
+        "make install"
+    ]
     
+    for command in commands:
+        subprocess.run(command.split(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        
 if __name__ == "__main__":
     main()
