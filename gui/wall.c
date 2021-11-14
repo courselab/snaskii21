6aa7c26f04ac6ba21ee8b8cc0731a5fb7fcd29f1
Diany <diany_press@usp.br>
2021-11-14 19:12:54 -0300

    Add Wall and Color data types

---
+++ gui/wall.c
@@ -0,0 +1,12 @@
+#include "wall.h"
+
+void initialize_wall(Wall* wall, Color* color, float topLeftX, float topLeftY, float width, float height) {
+    wall->color.red = color->red;
+    wall->color.green = color->green;
+    wall->color.blue = color->blue;
+    wall->color.alpha = color->alpha;
+    wall->shape.x = topLeftX;
+    wall->shape.y = topLeftY;
+    wall->shape.w = width;
+    wall->shape.h = height;
+}
