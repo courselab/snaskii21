8bf9276cc124132adb47d455349bd53c5f79dbfd
M-A-rs <araujo_m_@hotmail.com>
2021-12-01 22:24:08 -0300

    Add first Snake initializer function

---
+++ gui/snake.c
@@ -0,0 +1,8 @@
+#include "snake.h"
+
+void initialize_snake(Snake* snake, int headX, int headY, int blockSize) {
+    snake->headColor = (SDL_Color) {51, 0, 51, 0};
+    snake->bodyColor = (SDL_Color) {0, 102, 0, 0};
+    snake->headPosition = (SDL_Point) {headX, headY};
+    snake->size = blockSize;
+}
---
+++ gui/snake.h
@@ -0,0 +1,16 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include "SDL.h"
+
+typedef struct {
+    SDL_Color headColor;
+    SDL_Color bodyColor;
+    SDL_Point headPosition;
+    int size;
+    // SDL_Point bodyParts[1000]?
+} Snake;
+
+void initialize_snake(Snake* snake, int headX, int headY, int blockSize);
+
+#endif // SNAKE_H
