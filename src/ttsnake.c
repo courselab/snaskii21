eb452595dd38e22148165da489fdcb56f3e0a741
FelipeMoleiro <felipemoleiro@hotmail.com>
2022-01-10 19:47:05 -0300

    making colors stronger

--- src/ttsnake.c
+++ src/ttsnake.c
@@ -973,8 +973,8 @@ int main (int argc, char **argv) {
 		use_default_colors();
 		init_color(COLOR_BLACK, 0, 0, 0);
 		init_color(COLOR_WHITE, 1000,1000, 1000);
-		init_color(COLOR_GREEN, 0, 700, 0);
-		init_color(COLOR_RED, 700,0, 0);
+		init_color(COLOR_GREEN, 0, 1000, 0);
+		init_color(COLOR_RED, 1000,0, 0);
 		init_pair(1,COLOR_GREEN,-1);
 		init_pair(2,COLOR_RED,-1);
 	}
